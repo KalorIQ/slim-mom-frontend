ca535c7de85bf84638b4906896cc233e5b01156b
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2025-05-16 15:30:40 +0300

    created login page sc

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,46 @@
-const LoginPage = () =>  {
+import { Formik, Field, Form } from 'formik';
+import style from './LoginPage.module.css';
+
+const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <p>Please enter your credentials to log in.</p>
+    <div className={style.container}>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values) => {
+          console.log('Form values:', values);
+        }}
+      >
+        <Form className={style.form}>
+          <h2 className={style.title}>Log In</h2>
+          <div className={style.inputContainer}>
+            <Field
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                className={style.input}
+            />
+
+            <Field
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                className={style.input}
+            />
+          </div>
+        <div className={style.btnContainer}>
+            <button type="submit" className={style.logInButton}>
+                Log In
+            </button>
+            <button type="button" className={style.registerButton}>
+                Register
+            </button>
+        </div>
+        </Form>
+      </Formik>
     </div>
   );
-}
+};
+
 export default LoginPage;
